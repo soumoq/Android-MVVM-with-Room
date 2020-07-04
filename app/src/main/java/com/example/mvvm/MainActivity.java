536d620718a89e5051490319cc64bae3740ca097
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-07-04 13:10:58 +0530

    add AddNoteActivity

--- app/src/main/java/com/example/mvvm/MainActivity.java
+++ app/src/main/java/com/example/mvvm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mvvm;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -7,13 +8,18 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int ADD_NOTE_REQUEST = 1;
     private NoteViewModel noteViewModel;
 
     @Override
@@ -21,11 +27,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView=findViewById(R.id.recycler_view);
+        FloatingActionButton buttonAddNode = findViewById(R.id.button_add_note);
+        buttonAddNode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
+            }
+        });
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
 
-        final NoteAdopter adopter=new NoteAdopter();
+        final NoteAdopter adopter = new NoteAdopter();
         recyclerView.setAdapter(adopter);
 
 
@@ -38,5 +53,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note=new Note(title,description,priority);
+            noteViewModel.insert(note);
+
+            Toast.makeText(this,"Note saved",Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(this,"Note not saved",Toast.LENGTH_LONG).show();
+        }
+    }
 }
